8f9874288d8c5d61c5d1704e693de871764b98c6
KennyKramer <65318910+KennyKramer@users.noreply.github.com>
2022-05-09 20:52:10 -0300

    Update  routes/admin/novedades.js

--- novedades.js
+++ novedades.js
@@ -16,5 +16,25 @@ router.get('/agregar', (req, res, next) => {
     layout: 'admin/layout'
   });
 });
+// Controlador de captura y control de datos
+router.post('/agregar', async (req, res, next) => {
+  try {
+    if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect('admin/novedades')
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true, .essage: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+    concole.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se cargo la novedad'
+    });
+  }
+});
 
 module.exports = router;
